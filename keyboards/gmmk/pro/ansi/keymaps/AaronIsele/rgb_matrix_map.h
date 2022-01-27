9581d6c03945578ab4621770b6de99d293d92eb6
AaronIsele <aaronisele@gmail.com>
2022-01-27 00:22:11 -0500

    Added comments

--- keyboards/gmmk/pro/ansi/keymaps/AaronIsele/rgb_matrix_map.h
+++ keyboards/gmmk/pro/ansi/keymaps/AaronIsele/rgb_matrix_map.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 Jonavin Eng
+/* Copyright 2021 Aaron Isele
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
